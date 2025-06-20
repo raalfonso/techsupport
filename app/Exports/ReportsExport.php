6b2f1ab68eccb4e00244ff615180c1167f10c0e9
Romnick A. Alfonso <raalfonso@bcdagroup.net>
2025-06-20 16:04:09 +0800

    change in report

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -3,27 +3,70 @@ namespace App\Exports;
 
 use App\Models\Report;
 use Maatwebsite\Excel\Concerns\FromCollection;
-
-class ReportsExport implements FromCollection
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+Use Maatwebsite\Excel\Concerns\WithMapping;
+use Carbon\Carbon;
+class ReportsExport implements FromCollection, WithHeadings, WithStyles, WithMapping
 {
+    private int $rowNumber = 1;
+
+    private function calculateResponseTime($response){
+      $diffInMinutes = \Carbon\Carbon::parse($response->request_datetime)->diffInMinutes(\Carbon\Carbon::parse($response->response_datetime));
+      if ($diffInMinutes >= 60){ 
+         return round($diffInMinutes / 60)." hours";
+        }else{ 
+        return round($diffInMinutes)." mins";
+        } 
+                                   
+    }
+    private function calculateResolveTime($response){
+      $diffInMinutes = \Carbon\Carbon::parse($response->response_datetime)->diffInMinutes(\Carbon\Carbon::parse($response->resolve_datetime));
+      if ($diffInMinutes >= 60){ 
+         return round($diffInMinutes / 60)." hours";
+        }else{ 
+        return round($diffInMinutes)." mins";
+        } 
+                                   
+    }
    public function collection()
     {
-        return Report::select('ticket_number', 'client_id', 'department_id')->get();
+        return Report::whereIn('status', ['Done'])->get();
     }
+    public function map($report): array
+    {
+        
 
+        return [
+            $this->rowNumber++, // Row number (starts at 1)
+            Carbon::parse($report->response_datetime)->format('F j, Y h:i A'),
+            $report->client->name, // you can also use $report->client->name if you have a client relation
+            $report->department?->title ?? 'N/A',
+            $report->issues->title,
+            $this->calculateResponseTime($report),
+            $this->calculateResolveTime($report),
+            $report->resolve->user->name,
+        ];
+    }
     public function headings(): array
     {
         return [
-            'Ticket Number',
+            '#',
+            'Request Date',
             'Name',
             'Department',
+            'Issues',
+            'Response Time',
+            'Resolve Time',
+            'Technical Staff'
         ];
     }
 
     public function styles(Worksheet $sheet)
     {
         // Set font size and color for header row
-        $sheet->getStyle('A1:C1')->applyFromArray([
+        $sheet->getStyle('A1:K1')->applyFromArray([
             'font' => [
                 'bold' => true,
                 'size' => 14,
@@ -36,12 +79,12 @@ class ReportsExport implements FromCollection
         ]);
 
         // Set font size for all data rows
-        $sheet->getStyle('A2:C' . ($sheet->getHighestRow()))
+        $sheet->getStyle('A2:K' . ($sheet->getHighestRow()))
               ->getFont()
               ->setSize(12);
 
         // Auto-size the columns
-        foreach (range('A', 'C') as $col) {
+        foreach (range('A', 'K') as $col) {
             $sheet->getColumnDimension($col)->setAutoSize(true);
         }
 
