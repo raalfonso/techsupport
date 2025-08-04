135dcfceac1f23309e216667de95e58630a2f9b0
Romnick A. Alfonso <raalfonso@bcdagroup.net>
2025-08-04 11:18:09 +0800

    crud-modal fixing

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -265,7 +265,7 @@
           </div>
       </div> 
      <!-- Main modal -->
-      <div id="login-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ">
+      <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ">
           <div class="relative p-4 w-full max-w-md max-h-full">
               <!-- Modal content -->
               <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
