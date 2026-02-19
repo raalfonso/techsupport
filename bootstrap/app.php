bf4358d31c973400fb63d8c3888055450b851221
Romnick A. Alfonso <raalfonso@bcdagroup.net>
2026-02-19 11:10:06 +0800

    google

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+         $middleware->trustProxies(at: '*');
        // Register route middleware here
         $middleware->alias([
             'employee.auth' => \App\Http\Middleware\Authenticate::class,
