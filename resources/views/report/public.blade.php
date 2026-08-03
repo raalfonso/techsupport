ff704c4a2cfc7764e7592c897fe717280f001caa
Romnick A. Alfonso <raalfonso@bcdagroup.net>
2026-08-03 14:55:17 +0800

    fixing the sounds

--- resources/views/report/public.blade.php
+++ resources/views/report/public.blade.php
@@ -39,7 +39,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         const notificationSound = new Audio('{{ asset('sounds/387533__soundwarf__alert-short.wav') }}');
-        const alertSound = new Audio('{{ asset('sounds/spiderman.mp3') }}');
+        const alertSound = new Audio('{{ asset('sounds/spiderman3.mp3') }}');
         notificationSound.loop = true;
         let isPlaying = false;
         
@@ -93,7 +93,7 @@
                     });
 
                     if (hasNewTicket) {
-                        playAlert(3);
+                        playAlert(1);
                     }
 
                     const newGrid = doc.querySelector('.grid');
